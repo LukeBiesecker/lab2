5c06f2ce5371edbf1a4b84431c2881361525497a
LukeBiesecker <91580693+LukeBiesecker@users.noreply.github.com>
2021-09-29 17:51:48 +0000

    callback

--- main.py
+++ main.py
@@ -16,22 +16,24 @@ GPIO.setup(s, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 pwm = GPIO.PWM(e, 1)
 pwmm = GPIO.PWM(n, 1)
 
-def mycallback(y):
+def mycallback(signal):
   pwm.start(0)
   pwmm.start(0)
   while 1:
-    if(y==1):
+    if(signal==y):
       for dc in range(101):
         pwm.ChangeDutyCycle(dc)
         sleep(0.01)
       for ic in range(101,0,-1):
         pwm.ChangeDutyCycle(ic)
-    if(y==0):
+        sleep(0.01)
+    if(signal==s):
       for pc in range(101):
         pwmm.ChangeDutyCycle(pc)
         sleep(0.01)
       for mc in range(101,0,-1):
         pwmm.ChangeDutyCycle(mc)
+        sleep(0.01)
 
 
 
@@ -39,8 +41,7 @@ try:
   GPIO.output(p,1)
   sleep(1)
   GPIO.output(p,0)
-  GPIO.wait_for_edge(y,GPIO.RISING)
-  GPIO.wait_for_edge(s,GPIO.FALLING)
+  sleep(1)
   GPIO.add_event_detect(y,GPIO.RISING,callback = mycallback, bouncetime = 100)
   GPIO.add_event_detect(s,GPIO.FALLING,callback = mycallback, bouncetime = 100)
 except KeyboardInterrupt:
